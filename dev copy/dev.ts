8726ce7c20d3f93d9fec20a996511c13c716cbaf
lutam <l.tamigio@gmail.com>
2024-08-04 18:53:15 +0100

    fix: remove dev folder

--- dev copy/dev.ts
+++
@@ -1,8 +0,0 @@
-import AstronomyLab from "../dist/bundle.js";
-
-console.log(AstronomyLab);
-
-const mars = new AstronomyLab.CelestialBody("mars");
-const today = new Date('2022-01-17');
-
-mars.getEphemerisAtDate(today);
